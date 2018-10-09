c4a16ee0d9198c6fa4a9ee8d913469760eced45a
JonPatrick99 <43153901+JonPatrick99@users.noreply.github.com>
2018-10-08 21:12:45 -0600

    Add files via upload

---
+++ ListProcessor.cpp
@@ -0,0 +1,50 @@
+//ListProcessor.cpp
+//Jon Caldwell
+//COSC 2030
+//Oct 1, 2018
+
+
+#include "LinkedList.h"
+
+
+int main()
+{
+	List cows;
+	cout << "cows list  :  " << cows << endl << endl;
+
+	cows.insertAsFirst(1.23);
+	cows.insertAsFirst(2.34);
+	cout << "cows list  :  " << cows << endl << endl;
+
+	cows.removeFirst();
+	cout << "cows list  :  " << cows << endl << endl;
+
+	List horses(cows);
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl << endl;
+
+	horses.removeFirst();
+	horses.insertAsFirst(5.67);
+	cows.insertAsFirst(6.78);
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl << endl;
+
+	List pigs;
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl;
+	cout << "pigs list  :  " << pigs << endl << endl;
+
+	pigs = cows;
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl;
+	cout << "pigs list  :  " << pigs << endl << endl;
+
+	pigs = horses;
+	cout << "cows list  :  " << cows << endl;
+	cout << "horses list:  " << horses << endl;
+	cout << "pigs list  :  " << pigs << endl << endl;
+
+	cout << "End of code" << endl;
+	system("pause");
+	return 0;
+}
